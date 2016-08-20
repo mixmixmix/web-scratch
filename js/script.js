63975af241c87db22be01d1ca18a42d395346472
M. Kundegorski <mikolaj.kundegorski@gmail.com>
2016-08-20 14:08:54 +0100

    google street view

--- js/script.js
+++ js/script.js
@@ -12,9 +12,14 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
-
+    var streetName = $('#street').val();
+    var cityName = $('#city').val();
+    var addr = streetName + ', ' + cityName;
     // YOUR CODE GOES HERE!
-
+    $greeting.text('Welcome to' + addr + "!" );
+    var imgSrc='http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + addr ;
+    $body.append('<img class="bgimg" src="' + imgSrc + '">'); 
+    $body.append('SCRIPT WORKED'); 
     return false;
 };
 
